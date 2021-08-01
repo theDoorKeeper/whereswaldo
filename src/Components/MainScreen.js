0fdee32e17b8c61fb8290a408200475d145d5826
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-01 03:38:45 +0100

    moved the logic inside the component

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import styled from 'styled-components';
 import backGround from '../assets/backGround1.jpg'
+
+function MainScreen() {
+
 const Screen = styled.div`
 background-image : url(${backGround});
 height : 8422px;
+width : 100vw;
 cursor : crosshair;
 `
+
 const getCoordinates = (e)=>{
 
 const target = e.target;
@@ -18,7 +23,7 @@ const x = e.clientX - rect.left;
 const y = e.clientY - rect.top;
 console.log(x,y)
 }
-function MainScreen() {
+
     return (
         <Screen onClick={getCoordinates}>           
         </Screen>

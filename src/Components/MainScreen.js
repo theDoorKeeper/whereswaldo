55693b1bedcf4c078ffc7f1e5f812bb23bf545c1
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-01 22:14:11 +0100

    added mousePosition state

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import backGround from '../assets/backGround1.jpg'
+import CharMenu from './CharMenu';
 
 function MainScreen() {
-
-const Screen = styled.div`
-background-image : url(${backGround});
-height : 8422px;
-width : 100vw;
-cursor : crosshair;
-`
-
-const getCoordinates = (e)=>{
-
-const target = e.target;
+    const [mousePosition, setmMousePosition] = useState({});
 
 
-const rect = target.getBoundingClientRect();
 
 
-const x = e.clientX - rect.left;
-const y = e.clientY - rect.top;
-console.log(x,y)
-}
+    const Screen = styled.div`
+    background-image : url(${backGround});
+    height : 8422px;
+    width : 100vw;
+    cursor : crosshair;
+    `
+
+    const getCoordinates = (e)=>{
+
+    const target = e.target;
+
+
+    const rect = target.getBoundingClientRect();
+
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+        
+        setmMousePosition({x,y});
+        
+    }
 
     return (
-        <Screen onClick={getCoordinates}>           
+        <Screen onClick={getCoordinates}> 
+        <CharMenu/>          
         </Screen>
     )
 }

301f4236f3cdf945a59737ca2b61d858cb60e0fc
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-01 03:36:09 +0100

    added globalStyle and Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { createGlobalStyle } from "styled-components";
 import MainScreen from "./Components/MainScreen";
+import Navbar from "./Components/Navbar";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin : 0;
+    padding : 0;
+  } `
 
 function App() {
   return (
+    
     <div className="App">
+      <GlobalStyle/>
+      <Navbar/>
       <MainScreen/>
     </div>
+    
   );
 }
 

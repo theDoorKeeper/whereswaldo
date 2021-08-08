c45f4855ba2bfbda590aa008efa645060dc8d1cd
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-08 02:23:53 +0100

    aded sticky position to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,10 @@ import styled from 'styled-components'
     align-items : center;
     font-size : 2rem;
     margin-top : 0;
-    margin-bottom : 0;
+    margin-bottom : 0; 
+    position:sticky; 
+     top:0;
+    z-index : 1;
     
     `
 function Navbar(props) {

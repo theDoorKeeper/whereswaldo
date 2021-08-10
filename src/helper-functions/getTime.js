e8ea25c5202cd3c9ab3d68b7673d017fd3334f57
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-10 19:17:38 +0100

    fixed bug , added parhenteses on getHour etc ...

--- src/helper-functions/getTime.js
+++ src/helper-functions/getTime.js
@@ -2,9 +2,9 @@ import { timeStamp } from "../firebase"
 
         
         export const getTime = ()=>{
-             const getHours = timeStamp.getHours*3600;
-             const getMinutes = timeStamp.getMinutes*60;
-             const getSeconds = timeStamp.getSeconds;
-             
-             return getHours+getMinutes+getSeconds 
+             const getHour = timeStamp.getHours()*3600;
+             const getMinute = timeStamp.getMinutes()*60;
+             const getSecond = timeStamp.getSeconds();
+             const totalSeconds  = getHour+getMinute+getSecond
+             return totalSeconds
         }

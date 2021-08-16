296db4aa82a8c48aa6c5dc6bef6bc2e7e92fb88c
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-16 15:31:22 +0100

    removed jsx and styled components and replaced them with caroussel and diffucilty compoents

--- src/Components/DifficultyScreen.js
+++ src/Components/DifficultyScreen.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import Vash from '../assets/Vash.webp'
-import En from '../assets/En.png'
-import Dio from '../assets/Dio.webp'
 import { db } from '../firebase';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import Medium from './Difficulties/Medium';
+import Easy from './Difficulties/Easy'
 
 const Overlay = styled.div.attrs(props => ({
     style: {
-        display : props.visible ? 'flex' : 'none',
+        display : props.visible ? 'auto' : 'none',
          },
      }))`
     position: fixed; 
-    justify-content :center;
     width: 100%; 
     height: 100%; 
     top: 0;
@@ -23,69 +23,6 @@ const Overlay = styled.div.attrs(props => ({
     cursor: pointer; 
   
   `
-
-const DifficultyMenu = styled.div`
-    height : 700px;
-    width  : 800px;
-    background : #292d3e ;
-    border-radius : 30px;
-    margin-top : 3rem;
-    cursor  : auto;
-    display : flex ;
-    flex-direction : column;
-    justify-content : center;
-    align-items : center;
-`
-
-const CharList = styled.ul`
-    display : flex;
-    flex-direction : column;
-    gap : 0.8rem;
-    width : 80%;
-
-
-`
-
-const Char = styled.li`
-    width : 100%;
-    display : flex ;
-    justify-content :space-between;
-    font-size : 3rem;
-     cursor : pointer ;
-     color : #191818;
-     border-bottom : grey 1px solid ;
-    &:hover{
-        color : #9b1427;
-    }
-
-
-`
-
-const CharImg = styled.img`
-    height : 12rem;
-    &:hover{
-    scale : 1.2;
-    }
-   
-`
-
-const DiffcultyBtn = styled.button`
-    height : 3rem;
-    width : 20%;
-    border-radius : 30px;
-    border : none ;
-    background: #00ff41;
-    color : #191818  ;
-    font-size : 2rem;
-    font-weight : bold;
-    text-align : center ;
-    cursor : pointer ;
-    &:hover{
-     background : #008f11;
-     scale : 1.05;
-    }
-
-`
 
 function DifficultyScreen(props) {
     const {setDbChars, setisDoneLoading} = props;
@@ -112,23 +49,11 @@ function DifficultyScreen(props) {
     }, [difficulty])
     
     return (
-        <Overlay visible={visible}>
-            <DifficultyMenu>
-                <DiffcultyBtn onClick={handleClick}>Easy</DiffcultyBtn>
-                <CharList>
-                <Char>
-                <h4>En</h4> <CharImg src={`${En}`}/>
-                </Char>
-
-                <Char>
-                <h4>Vash</h4> <CharImg src={`${Vash}`}/>
-                </Char>
-
-                <Char>
-                <h4>Dio</h4> <CharImg src={`${Dio}`}/>
-                </Char>
-                </CharList>
-             </DifficultyMenu>   
+        <Overlay visible={visible} >
+            <Carousel showArrows={true}   >
+            <Easy handleClick={handleClick}/>
+            <Medium handleClick={handleClick}/>
+            </Carousel>
         </Overlay>
     )
 }

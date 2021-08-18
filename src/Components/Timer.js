f33734070c52094c74d70f1fef735fd9d94ab3b1
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-18 18:49:37 +0100

    changed the funcion to better display the digits

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import secondsToHms from '../helper-functions/convertHMS';
 
 function Timer() {
     const [time, setTime] = useState('00:00:00');
+    const [sec, setSec] = useState(0);
 
+    useEffect(() => {
+         
+        setTimeout(()=>{
+           setSec(previousState => {
+               return previousState+1
+           });
+            setTime(secondsToHms(sec));
+        },1000)
+    }, [sec])
     return (
         <div>
-            
+            {time}
         </div>
     )
 }
--- src/helper-functions/convertHMS.js
+++ src/helper-functions/convertHMS.js
@@ -1,11 +1,11 @@
-export default function convertHMS(value) {
-    const sec = parseInt(value, 10); 
-    let hours   = Math.floor(sec / 3600);
-    let minutes = Math.floor((sec - (hours * 3600)) / 60); 
-    let seconds = sec - (hours * 3600) - (minutes * 60);
+export default function  secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return hours+':'+minutes+':'+seconds; 
+    var hDisplay = h > 0 ? "0" + h + ":"  : "00:";
+    var mDisplay = m > 0 ? (m >= 10 ? m+":" : "0" + m + ":") : "00:" ;
+    var sDisplay = s > 0 ?( s >= 10 ? s : "0" + s ): "00";
+    return hDisplay + mDisplay + sDisplay; 
 }

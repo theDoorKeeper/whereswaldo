da19cf88a44266657e6d4c4c973d697973b483e6
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-18 17:56:17 +0100

    added time state

---
+++ src/Components/Timer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Timer() {
+    const [time, setTime] = useState('00:00:00');
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Timer

cccdb0a17c39f46e9e82c09eb70631b57051357a
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-19 21:09:26 +0100

    added go right and go left methods

--- src/Components/Caroussel.js
+++ src/Components/Caroussel.js
@@ -4,7 +4,24 @@ import React from 'react '
 function Caroussel() {
      const [componentNumber, setComponentNumber] = useState(1) 
 
-    
+     const goRight = () =>{
+         setComponentNumber(prev=>{
+             if (prev+1 > 2 ){
+                 return prev
+             }
+             return prev+1
+         })
+     };
+
+     const goLeft = () =>{
+        setComponentNumber(prev=>{
+            if (prev-1 < 1 ){
+                return prev
+            }
+            return prev-1
+        })
+    };
+
     return (
         <div>
             

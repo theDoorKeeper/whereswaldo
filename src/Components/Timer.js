72bd41f4f3609209868f9969a8a97ad2c59d8272
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-19 01:01:54 +0100

    received isgameoer prop

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from 'react'
 import secondsToHms from '../helper-functions/convertHMS';
 
 function Timer(props) {
-    const {isDoneLoading} = props ;
+    const {isDoneLoading, isGameOver} = props ;
     const [time, setTime] = useState('00:00:00');
     const [sec, setSec] = useState(0);
 
     useEffect(() => {
-         
+           if (isDoneLoading){
         setTimeout(()=>{
            setSec(previousState => {
                return previousState+1
            });
             setTime(secondsToHms(sec));
         },1000)
-    }, [sec])
+              }
+
+    }, [sec,isDoneLoading])
     return (
         <div>
             {time}

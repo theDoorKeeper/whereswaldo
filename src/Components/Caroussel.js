b37e341c60d50186bfd2d0c7d3a03f50f47a0dd2
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-19 21:07:08 +0100

    added componentNumber state

---
+++ src/Components/Caroussel.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react '
+
+function Caroussel() {
+     const [componentNumber, setComponentNumber] = useState(1) 
+
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Caroussel
